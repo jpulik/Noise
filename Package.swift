d854d83656e0540f569cde5e03b02b649a48d988
Jaroslav Pulik <jaroslav.pulik@gmail.com>
2021-12-02 09:33:40 +0100

    Package.swift updated with OpenSSL dependency

--- Package.swift
+++ Package.swift
@@ -7,12 +7,19 @@ let package = Package(
     name: "Noise",
     platforms: [
         .macOS(.v10_10), .iOS(.v8)
-        ],
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Noise",
             targets: ["Noise"]
+        )
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(
+            url: "https://github.com/OuterCorner/OpenSSL.git",
+            from: "1.1.1"
         )
     ],
     targets: [
